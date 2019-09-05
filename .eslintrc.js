b837f601d3cd8d7f5d53cbdc706c6d0ab8428734
zhibi <1017866168@qq.com>
2019-09-05 16:23:42 +0800

    update 更新eslint配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: ['plugin:vue/essential', '@vue/prettier', "plugin:vue-libs/recommended"],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'camelcase': [0, {
+      'properties': 'always'
+    }],
+    "vue/name-property-casing": ["error", "PascalCase"],
+    'space-before-function-paren': [2, 'never'],
   },
   parserOptions: {
     parser: 'babel-eslint'
